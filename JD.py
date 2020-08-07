a993efe4826e2790075a0cb0d0e7614949d483bc
fendoudexiaofeixia <l>
2020-08-07 22:49:38 +0800

    此版本需解决异步加载过程中爬取失败的问题

--- JD.py
+++ JD.py
@@ -6,72 +6,49 @@ import re
 import json
 import time
 import urllib3
-from urllib.request import urlopen
+import lxml
 
 
-def get_page_html(i=1):
+def get_page_front_html(i):
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    url = 'https://search.jd.com/Search?keyword=freebuds3&suggest=1.def.0.V12--38s0&wq=freebuds3&page=%s&s=%s&click=0' % (
-        i, i + 60)
+    url = 'https://search.jd.com/s_new.php?keyword=freebuds3&suggest=1.his.0.0&wq=freebuds3&scrolling=y&log_id=1596809916583.3068&tpl=3_M&isList=0&show_items=&page=' + str(
+        2 * i - 1)
     kv = {'User-Agent': 'Mozilla/5.0',
           'Referer': 'https://search.jd.com/Search?keyword=freebuds&enc=utf-8&wq=freebuds&pvid=a16fe2a910b14e7695ce194f60c490e5'}
-    # for i in range(1, 10, 2):
     resp = requests.get(url, headers=kv, verify=False)
     if resp.status_code == 200:
-        bs0bj = BeautifulSoup(resp.content, features='html.parser')
-        # time.sleep(1)
-        # with open(r'C:\Users\lixiaodong\Desktop\aaa.txt', 'w', encoding='utf8') as wstream:
-        #     wstream.write(str(bs0bj))
+        bs0bj = BeautifulSoup(resp.content, features='lxml')
         return bs0bj
-        # prices_id = re.match(_.attrs['id'], '[0-9]*')
-        # print(prices_id)
 
 
-def get_product_id(html):
-    product_id = html.findAll('img', {'class': "err-product"})
-    for _ in product_id:
-        if 'data-sku' and 'class' in _.attrs:
-            # print(re.findall(r'[0-9]*', _.attrs['id'])[6])
-            # return re.findall(r'[0-9]*', _.attrs['id'])[-2]
-            # return _.attrs['data-sku']
-            yield _.attrs['data-sku']
+def get_page_last_html(i):
+    a = time.time()
+    b = '%.5f' % a
+    url = 'https://search.jd.com/s_new.php?keyword=freebuds3&suggest=1.his.0.0&wq=freebuds3&scrolling=y&tpl=3_M&isList=0&show_items=&page=' + str(
+        2 * i) + '&s=' + str(48 * i - 20) + '&log_id=' + str(b)
+    kv = {'User-Agent': 'Mozilla/5.0',
+          'Referer': 'https://search.jd.com/Search?keyword=freebuds&enc=utf-8&wq=freebuds&pvid=a16fe2a910b14e7695ce194f60c490e5'}
+    resp = requests.get(url, headers=kv, verify=False)
+    if resp.status_code == 200:
+        bs0bj = BeautifulSoup(resp.content, features='lxml')
+        return bs0bj
 
 
-def get_prices(product_id):
-    js = requests.get('https://p.3.cn/prices/mgets?skuIds=J_' + product_id)
-    resp = json.loads(js.content)
-    return resp[0]['p']
-
-
-def page_decode(page_bytes, charsets):
-    for charset in charsets:
+def get_id(html):
+    product = html.findAll('li', {'class': 'gl-item'})
+    for data in product:
         try:
-            return page_bytes.decode(charset)
-        except UnicodeDecodeError:
-            pass
-
-
-def main():
-    rep = []
-    for i in range(1, 10, 2):
-        a = get_page_html(i)
-        for n in get_product_id(a):
-            rep.append(n)
-            print(n)
-            # print(get_prices(n))
-    # # print(b)
-
-    # print(a)
-    # print('********************************************************', i)
-    dic = {}.fromkeys(rep)
-    # # 这种方法建立字典，会把列表里的元素当做字典的键，由于字典的键不能重复，所以会自动去重
-    if len(dic) == len(rep):
-        print('列表里的元素互不重复！')
-    else:
-        print('列表里有重复的元素！')
-        print(len(dic), len(rep))
-
+            yield re.findall(r'[0-9]*\.00', str(data.findAll('div', {'class': 'p-price'})))[0]
+        except:
+            print('失败')
 
 
 if __name__ == '__main__':
-    main()
+    for _ in range(1, 111):
+        res = get_page_front_html(_)
+        res1 = get_page_last_html(_)
+        for i in get_id(res):
+            print(i)
+        for n in get_id(res1):
+            print(n)
+        print('--------------------第%s页-------------------------------------------' % _)
