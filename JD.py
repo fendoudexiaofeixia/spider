0ab818350ae0e0a7c9703a671f03b443131010bb
fendoudexiaofeixia <l>
2020-08-09 23:13:09 +0800

    下一步增加异步加载问题！

--- JD.py
+++ JD.py
@@ -8,48 +8,41 @@ import time
 import urllib3
 import lxml
 
+PRODUCT_ID = re.compile(r'<strong class="[A-Z]\_[0-9]*" data-done="1">')
+PRICES = re.compile(r'<i>[0-9]*.00</i>')
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def get_page_front_html(i):
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    url = 'https://search.jd.com/Search?keyword=freebuds3&suggest=1.his.0.0&wq=freebuds3&s=1&click=0&page=' + str(
-        2 * i - 1)
+
+def get_page_front_html():
+    url = 'https://search.jd.com/Search?keyword=freebuds3&suggest=1.his.0.0&wq=freebuds3&page=1&s=53&click=0'
     kv = {'User-Agent': 'Mozilla/5.0',
-          'Referer': 'https://search.jd.com/Search?keyword=freebuds&enc=utf-8&wq=freebuds&pvid=a16fe2a910b14e7695ce194f60c490e5'}
+          'Referer': 'https://www.jd.com/'}
     resp = requests.get(url, headers=kv, verify=False)
     if resp.status_code == 200:
         bs0bj = BeautifulSoup(resp.content, features='lxml')
         return bs0bj
 
 
-def get_page_last_html(i):
-    a = time.time()
-    b = '%.5f' % a
-    url = 'https://search.jd.com/s_new.php?keyword=freebuds3&suggest=1.his.0.0&wq=freebuds3&scrolling=y&tpl=3_M&isList=0&show_items=&page=' + str(
-        2 * i) + '&s=' + str(26 * i + 1) + '&log_id=' + str(b)
-    kv = {'User-Agent': 'Mozilla/5.0',
-          'Referer': 'https://search.jd.com/Search?keyword=freebuds&enc=utf-8&wq=freebuds&pvid=a16fe2a910b14e7695ce194f60c490e5'}
-    resp = requests.get(url, headers=kv, verify=False)
-    if resp.status_code == 200:
-        bs0bj = BeautifulSoup(resp.content, features='lxml')
-        return bs0bj
 
 
-def get_id(html):
-    product = html.findAll('li', {'class': 'gl-item'})
-    for data in product:
-        try:
-            time.sleep(0.01)
-            yield re.findall(r'[0-9]*\.00', str(data.findAll('div', {'class': 'p-price'})))[0]
-        except:
-            print('失败')
+def get_id(content, pattern):
+    product = pattern.search(str(content))
+    if product:
+        return re.findall(r'J_[0-9]*', product[0])[0]
+        # return product[0]
+
+
+def get_prices(number):
+    prices_url = requests.get('https://p.3.cn/prices/mgets?skuIds=' + str(number))
+    if prices_url.status_code == 200:
+        soup = json.loads(prices_url.content)
+        return soup[0]['p']
 
 
 if __name__ == '__main__':
-    for _ in range(1, 111):
-        res = get_page_front_html(_)
-        res1 = get_page_last_html(_)
-        for i in get_id(res):
-            print(i)
-        for n in get_id(res1):
-            print(n)
-        print('--------------------第%s页-------------------------------------------' % _)
+    a = get_page_front_html()
+    al = a.findAll('div', {'class': 'p-price'})
+    for i in al:
+        product_id = get_id(i, PRODUCT_ID)
+        prices = get_prices(product_id)
+        print('商品编号为：', product_id, '商品价格为：', prices)
