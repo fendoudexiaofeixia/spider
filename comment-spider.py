d1a6583a8ded2b3e4783e06a3f3f0a68d917875a
fendoudexiaofeixia <l>
2020-08-18 23:06:24 +0800

    多商品多页面爬取开发完成，下一步优化反扒机制

--- comment-spider.py
+++ comment-spider.py
@@ -7,13 +7,16 @@ import time
 import requests
 import json
 import random
+from jingdong import JD
+import threading
 
 
 # urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def get_comment_json(page=0):
-    url = 'https://club.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98&productId=66430683946&score=0&sortType=5&page=%s&pageSize=10&isShadowSku=0&fold=1' % page
+def get_comment_json(pid, page=0):
+    url = 'https://club.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98&productId=%s&score=0&sortType=5&page=%s&pageSize=10&isShadowSku=0&fold=1' % (
+        pid, page)
     kv = {'referer': 'https://item.jd.com/100013289660.html',
           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'}
     r = requests.get(url, headers=kv)
@@ -26,7 +29,37 @@ def get_comment_json(page=0):
             print(conten['content'])
 
 
+def get_pid1():
+    for pid in JD.return_id():
+        for i in range(1, 20, 2):
+            try:
+                get_comment_json(pid[2:], i)
+                time.sleep(random.random() * 5)
+                print('-------------------这是线程   1-------------------')
+            except:
+                print('爬取失败')
+    # return 0
+
+
+def get_pid2():
+    for pid in JD.return_id():
+        for i in range(2, 20, 2):
+            try:
+                get_comment_json(pid[2:], i)
+                time.sleep(random.random() * 5)
+                print('--------------------这是线程  2--------------------')
+            except:
+                print('爬取失败')
+    # return 0
+
+
 if __name__ == '__main__':
-    for i in range(100):
-        get_comment_json(i)
-        time.sleep(random.random() * 5)
+    t1 = threading.Thread(target=get_pid1, args=())
+    t2 = threading.Thread(target=get_pid2, args=())
+    # t3 = threading.Thread(target=main, args=())
+    t1.start()
+    t2.start()
+    # t3.start()
+    t1.join()
+    t2.join()
+    # t3.join()
